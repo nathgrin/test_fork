a4bfcfe27d9aac0d0de2d39a76b41caf532fb387
Mark Kooijman <kooi.barlaeus@gmail.com>
2023-02-03 20:39:17 +0100

    split into modules

--- src/make_test.py
+++ src/make_test.py
@@ -1,86 +1,8 @@
 import numpy as np
 
-
-def new_exercise(exc_name):
-
-    return r"""\newexercise{%s}\setcounter{question}{0}""" % (exc_name)
-
-
-class file_class():
-    fname = ""
-
-    def __init__(self, fname):
-        self.fname = fname
-
-    def clearfile(self):
-        open(self.fname, 'w').close()
-
-    def write_appline(self, writeline):
-        with open(self.fname, 'a') as thefile:
-            thefile.write(writeline)
-
-
-class test_class(list):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def add_question(self, question):
-        self.append(question)
-
-    def __add__(self, *args, **kwargs):  # Overwrite to return the proper class
-        return generation_class(super().__add__(*args, **kwargs))
-
-    def serialize(self, key: str) -> str:
-        return [entry[key] for entry in self]
-
-    def tex_preamble(self, exc_name, clearpage=False, vfill=False):
-        out = "\n%%% NEW\n"
-        if clearpage:
-            out += r"""\clearpage"""
-            out += "\n"
-        if vfill:
-            out += r"""\vfill"""
-            out += "\n"
-        out += new_exercise(exc_name)
-        out += "\n"
-        return out
-
-    def question_order(self, random_order: bool = True):
-        if random_order:
-            return np.random.permutation(len(self))
-        else:
-            return range(len(self))
-
-
-def new_test(*args, **kwargs):
-    return test_class(*args, **kwargs)
-
-
-class question_class(dict):
-    def __init__(self, *args, **kwargs):
-        super().__init__(self, *args, **kwargs)
-        self['text'] = kwargs.get('text', "")
-        self['options'] = kwargs.get('options', [])
-        self['n'] = (len(o) for o in self['options'])  # doesnt work
-
-    def set_options(self, options):
-        self['options'] = options
-        self['n'] = (len(o) for o in self['options'])  # doesnt work
-
-    def generate_text(self):
-
-        inds = [np.random.randint(len(opt)) for opt in self['options']]
-        print(self['text'])
-        thingies = [self['options'][i][ind] for i, ind in enumerate(inds)]
-        print(thingies)
-        out = self['text'].format(*thingies)
-        # print(">",out)
-        return out
-
-
-def new_question(*args, **kwargs):
-    return question_class(*args, **kwargs)
+from package_name.question import new_question
+from package_name.test import new_test
+from package_name.file import file_class
 
 
 def main():
